b8ff7e4f2808ca2d54c9e18e6c8f9464e2f4a68a
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2023-10-30 23:33:36 +0530

    added actions

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "cert-file.cer" ubuntu@https://ec2-18-170-224-59.eu-west-2.compute.amazonaws.com/  "sudo bash ~/deploy.sh"
